13ee0a4436f0c677ec2fdcf395199c39ecae7936
PARTHIPAN M <101990386+mmmpartha@users.noreply.github.com>
2023-04-11 16:17:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use("/api/v1", payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("https://ecommerce-mern-boa3.onrender.com", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
