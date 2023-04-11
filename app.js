80333546bb65aaa12632ae4c216ec5f7c7080e99
PARTHIPAN M <101990386+mmmpartha@users.noreply.github.com>
2023-04-11 12:56:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-const cors = require('cors');
-
 
 const errorMiddleware = require("./middleware/error");
 
@@ -38,15 +36,5 @@ app.get("*", (req, res) => {
 
 // Middleware for Errors
 app.use(errorMiddleware);
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200
-}
-app.use(cors(corsOptions))
-
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.set('trust proxy', 1);
 
 module.exports = app;
