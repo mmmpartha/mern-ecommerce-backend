999a498a21f33cd6474b99b5e41c5fba9df9ebeb
PARTHIPAN M <101990386+mmmpartha@users.noreply.github.com>
2023-04-24 15:29:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "https://ecommerce-mern-boa3.onrender.com", "build")));
 
 app.get("*", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "https://ecommerce-mern-boa3.onrender.com", "build", "index.html"));
 });
 
 
